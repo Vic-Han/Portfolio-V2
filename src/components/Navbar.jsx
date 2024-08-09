dde1de8612c3c130f267c7f99647e9373b85185f
Victor Han <victor.han72@gmail.com>
2024-08-09 10:39:49 -0600

    Added more project images are redid navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import './Navbar.css';
 
 // Navbar component
 // This component renders the navigation bar with buttons to toggle between different screens.
 function Navbar({clickHandler, darkmode}) {
-    console.log(darkmode)
+    const dark = 'text-white hover:bg-gray-700';
+    const light = 'text-black hover:bg-gray-300';
+    const styles = 'rounded-lg p-4 text-xl cursor-pointer font-bold';
     return (
-        <div className={`navbar ${darkmode ? 'dark' : 'light'}`}>
-            <div className={`navbar-btn ${darkmode ? 'dark' : 'light'} `} onClick={() => clickHandler('settings')}> Settings </div>    
-            <div className={`navbar-btn ${darkmode ? 'dark' : 'light'} `} onClick={() => clickHandler('mainpage')}> Mainpage </div>
-            <div className={`navbar-btn ${darkmode ? 'dark' : 'light'} `} onClick={() => clickHandler('projects')}> Projects </div>
-            <div className={`navbar-btn ${darkmode ? 'dark' : 'light'} `} onClick={() => clickHandler('resume')}> Resume </div>
-            <div className={`navbar-btn ${darkmode ? 'dark' : 'light'} `} onClick={() => clickHandler('about')}> About </div>
+        <div className="flex flex-row justify-between py-3 px-10">
+            
+            <div className={`${styles} ${darkmode ? dark : light} `} onClick={() => clickHandler('settings')}> Settings </div>    
+            <div className={`${styles} ${darkmode ? dark : light} `}  onClick={() => clickHandler('mainpage')}> Mainpage </div>
+            <div className={`${styles} ${darkmode ? dark : light} `}  onClick={() => clickHandler('projects')}> Projects </div>
+            <div className={`${styles} ${darkmode ? dark : light} `}  onClick={() => clickHandler('resume')}> Resume </div>
+            <div className={`${styles} ${darkmode ? dark : light} `} onClick={() => clickHandler('about')}> About </div>
 
         </div>
     );
--- src/data/Projects.js
+++ src/data/Projects.js
@@ -2,9 +2,9 @@ import image1 from '../assets/personal1.jpg'
 import image2 from '../assets/personal2.jpg'
 
 // import MyUC2 from '../assets/MyUC2.png'
-// import SpaceBook from '../assets/SpaceBook.png'
-// import TwitterBot from '../assets/TwitterBot.png'
-// import EsportsZone from '../assets/EsportsZone.png'
+import SpaceBook from '../assets/SpaceBook.png'
+import TwitterBot from '../assets/TwitterBot.png'
+import EsportsZone from '../assets/EsportsZone.png'
 import PassnPlay from '../assets/PassnPlay.png'
 // import UltimateTicTacToe from '../assets/UltimateTicTacToe.png'
 
@@ -21,15 +21,15 @@ const projects = [
     {
         name: 'SpaceBook',
         description: 'A space themed social media platform. Includes key features with a space twist.',
-        image : image1,
+        image : SpaceBook,
         github: 'https://github.com/EugeneL11/SpaceBook',
         link: null,
         video : 'https://www.youtube.com/watch?v=JtTn-pMRXLI&t=229s',
     },
     {
         name: 'Twitter Bot Detection',
-        description: 'Detection of Twitter bots using machine learning. Uses a variety of features to cluster bots.',
-        image : image2,
+        description: 'Detection of Twitter bots related to the Russo-Ukraine conflict. Uses a variety of features to cluster bots.',
+        image : TwitterBot,
         github: 'https://github.com/Logan-PD/SENG-550-Project',
         link: "https://vic-han.github.io/pdf/SENG_550_Final_Project_Group_1.pdf",
         video : null,
@@ -38,7 +38,7 @@ const projects = [
     {
         name: 'Esports Zone',
         description: 'Exlcusive gaming media platform. Many features included and designed for gamers.',
-        image : image1,
+        image : EsportsZone,
         github: 'https://github.com/Vic-Han/Esports-Zone',
         link: null,
         video : 'https://www.youtube.com/watch?v=KEn2bstXrFY',
