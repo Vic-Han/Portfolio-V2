e4ae6202d3f33ef2c9627778ee0c1ac99c728f55
Victor Han <victor.han72@gmail.com>
2024-10-22 17:56:18 -0600

    Minor code quality improvements for settings

--- src/components/AnimationToggle.jsx
+++ src/components/AnimationToggle.jsx
@@ -1,25 +1,19 @@
-import React,{ useState, useEffect } from 'react';
+import React from 'react';
 import './animationToggle.css'
 function AnimationToggle({value, change, dark}){
 
-    const [selected, setSelected] = useState(value);
-
-    const handleClick = (value) => {    
-        setSelected(value);
-        change(value);
-    }
     return (
-        <div className={"rating " + (dark ? 'dark' : '')}>
-            <input type="radio" id="star-1" name="star-radio" value={'Extreme'} checked={selected === 'Extreme'} onChange={(e) => handleClick(e.target.value)} />
-            <label htmlFor="star-1" onClick={() => handleClick(3)}>
+        <div className={`rating ${dark ? 'dark' : ''}`}>
+            <input type="radio" id="star-1" name="star-radio" value={'Extreme'} checked={value === 'Extreme'} onChange={(e) => change(e.target.value)} />
+            <label htmlFor="star-1">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
             </label>
-            <input type="radio" id="star-2" name="star-radio" value={'Normal'} checked={selected === 'Normal'} onChange={(e) => handleClick(e.target.value)} />
-            <label htmlFor="star-2" onClick={() => handleClick(2)} >
+            <input type="radio" id="star-2" name="star-radio" value={'Normal'} checked={value === 'Normal'} onChange={(e) => change(e.target.value)} />
+            <label htmlFor="star-2">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
             </label>
-            <input type="radio" id="star-3" name="star-radio" value={'Minimal'} checked={selected === 'Minimal'} onChange={(e) => handleClick(e.target.value)} />
-            <label htmlFor="star-3" onClick={() => handleClick(1)} >
+            <input type="radio" id="star-3" name="star-radio" value={'Minimal'} checked={value === 'Minimal'} onChange={(e) => change(e.target.value)} />
+            <label htmlFor="star-3">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path pathLength="360" d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"></path></svg>
             </label>
             
--- src/views/Settings.jsx
+++ src/views/Settings.jsx
@@ -4,7 +4,7 @@ import AnimationToggle from '../components/AnimationToggle';
 
 import ReadOnlyStars from '../components/ReadOnlyStars';
 import {GlobalContext} from '../context/GlobalContext';
-import { animateContentBoxes } from '../transitions/AnimationHelper';
+import {animateContentBoxes} from '../transitions/AnimationHelper';
 function Settings({close}) {
     const settingsStates = useContext(GlobalContext);
     const {darkMode, setDarkMode, animation, setAnimation} = settingsStates;
@@ -38,26 +38,26 @@ function Settings({close}) {
     return(
         <div className={`${darkMode ? 'bg-dark' : 'bg-light'} p-2 sm:p-4 xl:p-10 box-border w-screen h-screen fixed top-0 left-0 z-30`}>
           
-            <div className={`relative -translate-x-1/2 left-1/2 ${darkMode ? 'card-dark' : 'card-light'} rounded-xl my-2 sm:my-4 xl:my-10 p-3 sm:p-4 xl:p-6 3xl:p-10 w-11/12 sm:w-5/6 xl:w-2/3 text-center ${boxAnimationStates[0]}`}>
+            <div className={`relative -translate-x-1/2 left-1/2 rounded-xl my-2 sm:my-4 xl:my-10 p-3 sm:p-4 xl:p-6 3xl:p-10 w-11/12 sm:w-5/6 xl:w-2/3 text-center ${darkMode ? 'card-dark' : 'card-light'} ${boxAnimationStates[0]}`}>
                 <div className='flex flex-row'>
-                    <div className="w-1/2 p-0">
+                    <div className='w-1/2 p-0'>
                         <div className='relative left-0 w-fit'>
-                            <div className="text-sm sm:text-base xl:text-3xl 2xl:text-4xl 3xl:text-5xl text-primary w-fit mb-0.5 sm:mb-1 md:mb-2 lg-mb-3 xl:mb-4 2xl:mb-4 3xl:mb-6">Dark Theme?</div>
-                            <DarkThemeSwitch value={darkMode} change={setDarkMode}/>
+                            <div className='text-sm sm:text-base xl:text-3xl 2xl:text-4xl 3xl:text-5xl text-primary w-fit mb-0.5 sm:mb-1 md:mb-2 lg-mb-3 xl:mb-4 2xl:mb-4 3xl:mb-6'>Dark Theme?</div>
+                            <div className='scale-60 sm:scale-75 md:scale-95 lg:scale-105 2xl:scale-125 3xl:scale-150'><DarkThemeSwitch value={darkMode} change={setDarkMode}/></div>
                         </div>
                     </div>
                     <div className='w-1/2 p-0'>
                         <div className='relative left-full -translate-x-full w-fit'>
-                            <button onClick={close} className={`close-button w-10 sm:w-14 xl:w-20 2xl:w-24 3xl:w-28 4xl:w-32 ${closeButtonStyle(animation)}`}>  </button>
+                            <button onClick={close} className={`close-button w-10 sm:w-14 xl:w-20 2xl:w-24 3xl:w-28 4xl:w-32 ${closeButtonStyle(animation)}`}></button>
                         </div>
                     </div> 
                 </div>
-                <div className="flex flex-row -my-6 md:my-0 3xl:my-8">
+                <div className='flex flex-row -my-6 md:my-0 3xl:my-8'>
                     <div className='min-h-full w-1/3'>
                         <div className='relative left-0 w-fit top-1/2 -translate-y-1/2'>
                             <div className='text-sm sm:text-base xl:text-3xl 2xl:text-4xl 3xl:text-5xl mb-2 sm:mb-4 lg:mb-6 xl:mb-10 text-primary xl:-translate-x-0.5'>Animation Level:</div>
-                            <div className="text-secondary text-xs sm:text-base xl:text-xl 2xl:text-2xl 3xl:text-3xl"> {animation}</div>
-                            <div className={`relative -translate-x-1/2 left-1/2 p-0 sm:p-1 md:p-2 lg:p-3 xl:w-fit 3xl:p-8 scale-50 sm:scale-60 md:scale-95 lg:scale-110 xl:scale-125 3xl:scale-150`}> <AnimationToggle value={animation} change={setAnimation} dark={darkMode}/> </div>
+                            <div className='text-secondary text-xs sm:text-base xl:text-xl 2xl:text-2xl 3xl:text-3xl'> {animation}</div>
+                            <div className='relative -translate-x-1/2 left-1/2 p-0 sm:p-1 md:p-2 lg:p-3 xl:w-fit 3xl:p-8 scale-50 sm:scale-60 md:scale-95 lg:scale-110 xl:scale-125 3xl:scale-150'> <AnimationToggle value={animation} change={setAnimation} dark={darkMode}/> </div>
                         </div>
                     </div>
                     <div className='min-h-full p-0 lg:p-6 w-2/3 scale-50 sm:scale-60 md:scale-95 lg:scale-110 xl:scale-125 3xl:scale-150 xl:p-8'>
