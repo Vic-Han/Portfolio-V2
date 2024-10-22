67f2048c8b17bf734dd1f6b37a533096e89bc0fa
Victor Han <victor.han72@gmail.com>
2024-10-22 17:06:03 -0600

    Better code quality for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,37 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useEffect } from 'react';
 import Cookies from 'js-cookie';
-
 import Portfolio from './Portfolio';
-import { GlobalProvider } from './context/GlobalContext'
+import { GlobalProvider } from './context/GlobalContext';
+
+/**
+ * The main App component wraps the Portfolio component within the GlobalProvider.
+ * In addition, it checks for the presence of 'darkMode' and 'animationLevel' cookies and assigns default values if they are not found.
+ */
 function App() {
+  /**
+   * The useEffect checks for the presence of 'darkMode' and 'animationLevel' cookies.
+   * 
+   * 'darkMode' defaults to 'true' if not present.
+   * 'animationLevel' defaults to 'Normal' if not present.
+   */
   useEffect(() => { 
-    const darkTheme = Cookies.get('darkMode');
-    const animationLevel = Cookies.get('animationLevel'); 
-    if(!darkTheme){      
-      Cookies.set('darkMode', 'false');     
-    }    
-    else if(!animationLevel){      
-      Cookies.set('animationLevel', 'Low');
+    
+    if (!Cookies.get('darkMode')) {      
+      Cookies.set('darkMode', 'true')    
     }
+    if (!Cookies.get('animationLevel')) {      
+      Cookies.set('animationLevel', 'Normal')
+    }
+
   }, []); 
   
   return (
     <div className='w-screen h-screen overflow-x-hidden'>
-    <GlobalProvider>
-      <Portfolio/>
-    </GlobalProvider>
+      <GlobalProvider>
+        <Portfolio/>
+      </GlobalProvider>
     </div>  
   );
 }
   
-export default App; 
+export default App;
